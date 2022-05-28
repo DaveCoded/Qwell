4b66a3d0168dcaa8ebcd431accaf58dafdaa8379
Dave Bernhard <davidbernhard@live.com>
2022-05-28 17:47:28 +0100

    add more queries

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import prisma from './prisma/client';
 
 const startServer = async () => {
     const app = express();
@@ -8,8 +9,51 @@ const startServer = async () => {
 
     // The GraphQL schema
     const typeDefs = gql`
+        enum Mode {
+            WRITING
+            REVISING
+        }
+
         type Query {
             hello: String
+        }
+
+        type Query {
+            projects: [Project]
+        }
+
+        type Query {
+            project(id: Int!): Project
+        }
+
+        type Query {
+            documents: [Document]
+        }
+
+        type Query {
+            document(id: Int!): Document
+        }
+
+        type Project {
+            id: Int!
+            name: String!
+            description: String
+            currentCycle: Int!
+            currentMode: Mode!
+            createdAt: String!
+            updatedAt: String!
+            Document: [Document]
+        }
+
+        type Document {
+            id: String!
+            content: String!
+            createdAt: String!
+            updatedAt: String!
+            cycle: Int!
+            mode: Mode!
+            project: Project!
+            projectId: Int!
         }
     `;
 
@@ -17,7 +61,13 @@ const startServer = async () => {
     // e.g. Fetch a list of items from a database
     const resolvers = {
         Query: {
-            hello: () => 'Hello world!'
+            hello: () => 'Hello world!',
+            projects: async () => await prisma.project.findMany(),
+            documents: async () => await prisma.document.findMany(),
+            // project: async (id: number) => await prisma.project.findUnique({ where: { id } }),
+            // document: async (id: number) => await prisma.document.findUnique({ where: { id } })
+            project: async (_parent: any, args: any) =>
+                await prisma.project.findUnique({ where: { id: args.id } })
         }
     };
 
